e2980fd81878a7a8e4d9da26f1e00d2f87490773
Jake <bitbucket@jakelee.co.uk>
2017-01-29 01:13:55 +0000

    First draft pretty much fully functional

--- js/clear_history.js
+++ js/clear_history.js
@@ -1,20 +1,34 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.history.search(
-		{
-			text: '*google.com*', 
-			startTime: 0,
-			endTime: 2147485546999,
-			maxResults: 9999}, 
-		function(results) {
-			var itemsDeleted = 0;
-			for (itemsDeleted; itemsDeleted < results.length; itemsDeleted++) {
-				chrome.history.deleteUrl({ url: results[itemsDeleted].url });
-			}
-			
-			if (itemsDeleted > 0) {
-				alert("Deleted " + itemsDeleted + " items.");
-			} else {
-				alert("Nothing to delete!");
-			}
-		})
+    chrome.storage.sync.get({
+        sites: 'supersecretsite.com'
+    }, deleteBySites);
 });
+
+function deleteBySites(storage) {
+    alert(storage.sites);
+    var allSites = storage.sites.split("\n");
+    for (var i = 0; i < allSites.length; i++) {
+        deleteBySite(allSites[i]);
+    }
+}
+
+function deleteBySite(site) {
+    chrome.history.search({
+            text: site,
+            startTime: 0,
+            endTime: 2147485546999,
+            maxResults: 9999
+        },
+        function(results) {
+            var itemsDeleted = 0;
+            for (itemsDeleted; itemsDeleted < results.length; itemsDeleted++) {
+                chrome.history.deleteUrl({
+                    url: results[itemsDeleted].url
+                });
+            }
+
+            if (itemsDeleted > 0) {
+                alert("Deleted " + itemsDeleted + " items for: " + site);
+            }
+        })
+}
--- options.js
+++ options.js
@@ -1,23 +1,26 @@
 // Saves options to chrome.storage.sync.
 function save() {
-    var site = document.getElementById('site').value;
-    chrome.storage.sync.set({siteList: site}, function() {
-        var status = document.getElementById('status');
-        status.textContent = 'Sites saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 750);
-    });
+    var sitesToSave = document.getElementById('sites').value;
+    chrome.storage.sync.set({
+        sites: sitesToSave
+    }, displaySavedMessage);
 }
 
 function load() {
     chrome.storage.sync.get({
-        siteList: 'supersecretsite.com'
+        sites: 'supersecretsite.com'
     }, function(items) {
-        document.getElementById('site').value = items.siteList;
+        document.getElementById('sites').value = items.sites;
     });
 }
 
+function displaySavedMessage() {
+    var status = document.getElementById('status');
+    status.textContent = 'Sites saved.';
+    setTimeout(function() {
+        status.textContent = '';
+    }, 2000);
+}
+
 document.addEventListener('DOMContentLoaded', load);
-document.getElementById('save').addEventListener('click',
-        save);
+document.getElementById('save').addEventListener('click', save);
